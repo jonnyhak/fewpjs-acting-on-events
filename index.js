cbf423f09514985aacd71cf2b37fb43a48a78de2
jonnyhak <jonnyhak@gmail.com>
2020-11-17 16:23:50 -0500

    commit tree becb3f475bd2c269c6bb604b982718fdb7bb5f74

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+    console.log(left, "moving left")
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+//   console.log(rightNumbers)
+  let right = parseInt(rightNumbers, 10);
+    console.log(right, "moving right")
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }
+
+
+    // var leftNumbers = dodger.style.left.replace('px', '');
+    // var left = parseInt(leftNumbers, 10)
+    // if (left > 0) {
+    // dodger.style.left = `${left + 1}px`
+    // }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
